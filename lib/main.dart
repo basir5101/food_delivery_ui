cdc128817e2fa00c196b81a20e3d86ff230b559a
Abdul Basir <basir.bsmrstu@gmail.com>
2022-02-07 11:59:23 +0600

    admob add functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/ad_state.dart';
 import 'package:food_delivery_app/screens/home.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final initFuture = MobileAds.instance.initialize();
+  final adState = AdState(initFuture);
+  runApp(
+    Provider.value(
+      value: adState,
+      builder: (context, child) => const MyApp(),
+    )
+  );
 }
 
 
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/ad_state.dart';
 import 'package:food_delivery_app/custom_widgets/heading_text.dart';
 import 'package:food_delivery_app/custom_widgets/paragraph_text.dart';
 import 'package:food_delivery_app/custom_widgets/theme_button.dart';
 import 'package:food_delivery_app/screens/second_home.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,6 +15,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  late BannerAd banner;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final addState = Provider.of<AdState>(context);
+    addState.initialization.then((status) =>
+    {
+      setState(() {
+        banner = BannerAd(size: AdSize.banner,
+            adUnitId: addState.bannerAdUnitIt,
+            listener: BannerAdListener(
+
+              onAdLoaded: (Ad ad) => print('Ad loaded.'),
+              onAdFailedToLoad: (Ad ad, LoadAdError error) {
+                ad.dispose();
+                print('Ad failed to load: $error');
+              },
+              onAdOpened: (Ad ad) => print('Ad opened.'),
+              onAdClosed: (Ad ad) => print('Ad closed.'),
+              onAdImpression: (Ad ad) => print('Ad impression.'),
+            ),
+            request: AdRequest())..load();
+      }
+      )
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,8 +62,15 @@ class _HomeState extends State<Home> {
               HeadingText(heading: 'Find your  Comfort Food here'),
               const ParagraphText(
                 paragraph:
-                    'Here You Can find a chef or dish for every taste and color. Enjoy!',
+                'Here You Can find a chef or dish for every taste and color. Enjoy!',
               ),
+              if(banner == null)
+                SizedBox(height: 50,)
+               else
+                 Container(
+                   height: 50,
+                   child: AdWidget(ad: banner),
+                 )
             ],
           ),
         ),
